ff1421fbc3d2147d245d400a8956283e2f1e7307
LindaTancini <lindatancini95@gmail.com>
2025-04-11 14:20:19 +0200

    feat: exported and create function

---
+++ src/components/main.jsx
@@ -0,0 +1,15 @@
+// IMPORTAZIONI
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+// FUNZIONE CHE COMPRENDE IL COMPONENTE MAIN
+function Main() {
+  return (
+    <div>
+      <h1>Lista Attrici</h1>
+    </div>
+  );
+}
+
+// ESPORTAZIONI
+export default Main;
