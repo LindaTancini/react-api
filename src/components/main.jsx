3e9fa06de3d5ab5c8c8adde348ab1f053b5ace04
LindaTancini <lindatancini95@gmail.com>
2025-04-11 15:09:15 +0200

    feat: add useState

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -8,14 +8,16 @@ const endpoint = "https://www.freetestapi.com/api/v1/actresses";
 
 // FUNZIONE CHE COMPRENDE IL COMPONENTE MAIN
 function Main() {
-  //USO USE EFFECT PER CHIAMARE LA FUNZIONE E STAMPARE IN CONSOLE IL RISULTATO DELLA CHIAMATA API
+  // CREO VARIABILE USE STATE
+  const [actresses, setActresses] = useState([]);
+  // USO USE EFFECT PER CHIAMARE LA FUNZIONE E STAMPARE IN CONSOLE IL RISULTATO DELLA CHIAMATA API
   useEffect(() => {
     loadActress();
   }, []);
-
   // CREO FUNZIONE PER CARICARE LE API
   function loadActress() {
     axios.get(endpoint).then((result) => {
+      setActresses(result.data);
       console.log("Ho chiamato le API:", result.data);
     });
   }
