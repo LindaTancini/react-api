3c5a4c64cfa2f6c44a973b08d4b2b8b92ecbe6bf
LindaTancini <lindatancini95@gmail.com>
2025-04-11 14:53:10 +0200

    feat: create structure

---
+++ src/components/card.jsx
@@ -0,0 +1,16 @@
+//CREO FUNZIONE CARD CHE COMPRENDE LA SUA STRUTTURA HTML
+function Card() {
+  return (
+    <div>
+      <h2>Nome Attrice:</h2>
+      <p>Anno di Nascita:</p>
+      <p>Nazionalità:</p>
+      <p>Biografia:</p>
+      <p>Immagine:</p>
+      <p>Riconoscimenti:</p>
+    </div>
+  );
+}
+
+//ESPORTAZIONI
+export default Card;
