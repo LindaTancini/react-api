812bae547838707835a41ee2b8118cd1013058a5
LindaTancini <lindatancini95@gmail.com>
2025-04-11 14:41:20 +0200

    feat: complete Milestone 1

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,8 +2,23 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
+// PRENDO LE API E LE METTO DENTRO UNA VARIABILE ENDPOINT
+const endpoint = "https://www.freetestapi.com/api/v1/actresses";
+
 // FUNZIONE CHE COMPRENDE IL COMPONENTE MAIN
 function Main() {
+  //USO USE EFFECT PER CHIAMARE LA FUNZIONE E STAMPARE IN CONSOLE IL RISULTATO DELLA CHIAMATA API
+  useEffect(() => {
+    loadActress();
+  }, []);
+
+  // CREO FUNZIONE PER CARICARE LE API
+  function loadActress() {
+    axios.get(endpoint).then((result) => {
+      console.log("Ho chiamato le API:", result.data);
+    });
+  }
+
   return (
     <div>
       <h1>Lista Attrici</h1>
